5e885e4d10d9bd24bd8f096128511126b2a3de03
czubert@gmail.com <Zuzia2506!>
2019-08-08 12:49:51 +0200

    added hinting to teams module

--- teams.py
+++ teams.py
@@ -4,16 +4,16 @@ import characters
 
 
 class Team:
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         self.name = name
         self.team = []
         self.opponent_team = None
         self.generator_field = None
 
-    def __str__(self):
+    def __str__(self) -> str:
         return ' \n'.join([element.__str__() for element in self.team])
 
-    def add_character(self, hero):
+    def add_character(self, hero: object) -> None:
         """
         Adds character to a team and links character with it's team
         :param hero: Character object
@@ -21,18 +21,18 @@ class Team:
         self.team.append(hero)
         hero.team = self
 
-    def team_generator(self, number):
+    def team_generator(self, number: int) -> None:
         types_of_characters = [characters.Sorceress, characters.Warrior, characters.Support, characters.Voodoo]
         [self.add_character(random.choice(types_of_characters)()) for _ in range(number)]
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: int) -> object:  # TODO: is it integer for sure?
         return self.team.__getitem__(item)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.team)
 
     @staticmethod
-    def team_act(char):
+    def team_act(char) -> None:
         """
         Randoms hero from team, checks if it has modificators on himself (if yes act),
         checks if randomed char is support if yes, then looks for weakest hero to heal him,
@@ -53,19 +53,19 @@ class Team:
         char.next_move(target)
 
     # generator going through team list one by one, if it reaches the end it raise an error
-    def find_attacking_character(self):
+    def find_attacking_character(self) -> object:  # TODO is yield a return?
         # return random.choice(self.team)
         for char in self.team:
             yield char
         yield None
 
-    def find_strongest_character(self):
+    def find_strongest_character(self) -> None:
         """
         finds strongest character from team
         :return: character object
         """
         pass
 
-    def after_round_regenerate_mana_and_hp(self):
+    def after_round_regenerate_mana_and_hp(self) -> None:
         for char in self.team:
             char.regenerate()
